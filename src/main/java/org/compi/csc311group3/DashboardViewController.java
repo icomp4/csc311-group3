1298ee40d11698e8d63a3f39ec14a2aa4c01d855
NicholasPapazis <nicholasepapazis@gmail.com>
2024-10-31 14:48:56 -0400

    added dashboard controller file

--- src/main/java/org/compi/csc311group3/DashboardViewController.java
+++ src/main/java/org/compi/csc311group3/DashboardViewController.java
@@ -1,6 +1,62 @@
 package org.compi.csc311group3;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+
 public class DashboardViewController {
+
+    @FXML
+    private Text dashboardLink;
+
+    @FXML
+    private Text analyticsLink;
+
+    @FXML
+    private Text addExpenseLink;
+
+    @FXML
+    private Text addDepositLink;
+
+    @FXML
+    private Text settingsLink;
+
+
+    @FXML
+    void dashboardLinkClicked(ActionEvent event) {
+
+        // TODO: Implement dashboard functionality to navigate to dashboard page
+
+    }
+
+    @FXML
+    void analyticsLinkClicked(ActionEvent event) {
+
+        // TODO: Implement functionality to navigate to analytics page
+
+    }
+
+    @FXML
+    void addExpenseLinkClicked(ActionEvent event) {
+
+        // TODO: Implement functionality to navigate to addExpense page
+
+    }
+
+    @FXML
+    void addDepositLinkClicked(ActionEvent event) {
+
+        // TODO: Implement functionality to navigate to addDeposit page
+
+    }
+
+    @FXML
+    void settingsLinkClicked(ActionEvent event) {
+
+        // TODO: Implement functionality to navigate to settings page
+    }
+
+
 
 
 
