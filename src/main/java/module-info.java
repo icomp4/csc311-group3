e3937bdb5e441be8706c3a439e088deb61f64c4a
DanS-89 <stevens.j.daniel@gmail.com>
2024-10-30 22:35:58 -0400

    expense class, expense ui, and expense controller

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module org.compi.csc311group3 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
+    requires jdk.jfr;
 
 
     opens org.compi.csc311group3 to javafx.fxml;
